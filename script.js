5d19088e87624b0b4782cc55771ef9f6cd035143
tomascieska <tomas.cieska@gmail.com>
2023-11-23 21:22:47 +0000

    Initial commit

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const itemList = document.getElementById('item-list')
 const clearBtn = document.getElementById('clear')
 const itemFilter = document.getElementById('filter')
 
+function displayItems() {
+    const itemsFromStorage = getItemsFromStorage()
+    itemsFromStorage.forEach(item => additemToDOM(item))
+    checkUI()
+}
 
 
 function onAddItemSubmit(e){
@@ -40,22 +45,6 @@ function additemToDOM(item){
    itemList.appendChild(li)
 }
 
-function addItemToStorage (item){
-    let itemsFromStorage
-
-    if(localStorage.getItem('items') === null) {
-        itemsFromStorage = []
-    } else {
-        itemsFromStorage = JSON.parse(localStorage.getItem('items'))
-    }
-
-    //Add new item to array
-    itemsFromStorage.push(item)
-
-    //Convert to JSON string and set to Local Storage
-    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
-}
-
 function createButton(classes){
     const button = document.createElement('button')
     button.className = classes
@@ -68,6 +57,28 @@ function createIcon(classes){
     const icon = document.createElement('i')
     icon.className = classes
     return icon
+}
+
+function addItemToStorage (item){
+    const itemsFromStorage = getItemsFromStorage()
+
+    //Add new item to array
+    itemsFromStorage.push(item)
+
+    //Convert to JSON string and set to Local Storage
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+}
+
+function getItemsFromStorage(){
+    let itemsFromStorage
+
+    if(localStorage.getItem('items') === null) {
+        itemsFromStorage = []
+    } else {
+        itemsFromStorage = JSON.parse(localStorage.getItem('items'))
+    }
+
+    return itemsFromStorage
 }
 
 function removeItem(e) {
@@ -116,11 +127,16 @@ function checkUI (){
     }
 }
 
-//Event listeners
+//Initialize App
+function init(){
+    //Event listeners
 itemForm.addEventListener('submit', onAddItemSubmit)
 itemList.addEventListener('click', removeItem)
 clearBtn.addEventListener('click', clearItems)
 itemFilter.addEventListener('input', filterItems)
-
+document.addEventListener('DOMContentLoaded', displayItems)
 
 checkUI()
+}
+
+init()
